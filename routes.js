92ed8ad2a8fb1720b0c8609e49f63ca69e1b23eb
Aswath K <aswath.sana@gmail.com>
2017-03-11 01:59:36 +0530

    Random url added

--- routes.js
+++ routes.js
@@ -1,14 +1,15 @@
+const randstr = require('js-randstr');
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-    return res.redirect('/abcdef');
+    return res.redirect('/' + randstr(6));
 });
 
 router.get('/:room', (req, res) => {
     const room = req.params.room;
     if(room.length != 6)
         return res.redirect('/404.html');
-    res.sendFile(__dirname + '/client/index.html');
+    res.sendFile(__dirname + '/client/home.html');
 });
 
 module.exports = router;
