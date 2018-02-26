3c173621716b0e6eb624257e896182cef3bcdb38
Aswath K <aswath.sana@gmail.com>
2018-02-26 19:10:08 +0530

    refractor: add text tool

--- client/src/index.js
+++ client/src/index.js
@@ -2,22 +2,26 @@ import './style.css';
 
 import Pen from './tools/pen';
 import Eraser from './tools/eraser';
+import Text from './tools/text';
 import Pan from './tools/pan';
 
 const canvas = document.getElementById('canv');
 
 let pen = new Pen(canvas);
 let eraser = new Eraser(canvas);
+let text = new Text(canvas);
 let pan = new Pan(canvas);
 
 let deselectAll = () => {
   pen.deselect();
   eraser.deselect();
+  text.deselect();
   pan.deselect();
 };
 
 pen.onDeselect(deselectAll);
 eraser.onDeselect(deselectAll);
+text.onDeselect(deselectAll);
 pan.onDeselect(deselectAll);
 
 pen.select();
---
+++ client/src/tools/text.js
@@ -0,0 +1,68 @@
+import Tool from './tool';
+
+export default class Text extends Tool {
+  constructor(canvas) {
+    super(canvas);
+    this.typing = false;
+    let select = this.select.bind(this);
+    document.querySelector('.tool-item.text').addEventListener('click', select, false);
+  }
+
+  select() {
+    this.deselectAll();
+    this.canvas.classList.add('text');
+    document.querySelector('.tool-item.text').classList.add('active');
+
+    this.addClickEvents();
+  }
+
+  deselect() {
+    this.canvas.classList.remove('text');
+    document.querySelector('.tool-item.text').classList.remove('active');
+    this.removeEvents();
+    this.rasterize();
+  }
+
+  start(e) {
+    if(this.typing) {
+      this.rasterize();
+    } else {
+      let x, y;
+      if(e.touches) {
+        x = e.touches[0].clientX - this.canvas.offsetLeft;
+        y = e.touches[0].clientY - this.canvas.offsetTop;
+      } else {
+        x = e.clientX - this.canvas.offsetLeft;
+        y = e.clientY - this.canvas.offsetTop;
+      }
+
+      // Creating element for user input
+      let textPsuedo = document.createElement('input');
+      textPsuedo.type = 'text';
+      textPsuedo.classList.add('text-tool-accept');
+      document.body.appendChild(textPsuedo);
+      textPsuedo.style.left = x + 'px';
+      textPsuedo.style.top = y - 18.5 + 'px';
+      textPsuedo.style.color = '#000000';
+      textPsuedo.addEventListener('keydown', e => {
+        if(e.keyCode == 13)
+          this.rasterize();
+      });
+      setTimeout(() => textPsuedo.focus(), 100);
+      this.typing = true;
+      this.x = x;
+      this.y = y;
+    }
+  }
+
+  rasterize() {
+    let textPseudo = document.querySelector('.text-tool-accept');
+    if(textPseudo) {
+      this.ctx.fillStyle = '#000000';
+      this.ctx.font = '14px sans-serif';
+      this.ctx.fillText(textPseudo.value, this.x + 11, this.y + 6);
+      document.body.removeChild(textPseudo);
+      this.typing = false;
+    }
+  }
+}
